96e5a34e2b9d6b9e42cbcb9934ee175c8f053ddb
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-03-19 15:37:14 +0100

    First test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.kohsuke:github-api:1.123")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
+
+import org.kohsuke.github.GitHubBuilder
+import java.io.File
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val githubConfigPath = Paths.get(Paths.get(System.getProperty("user.dir")).toString(), ".githubconfig")
+    if (!File(githubConfigPath.toString()).exists()) {
+        println("No GitHub config file found, exiting.")
+        exitProcess(0)
+    }
+
+    val github = GitHubBuilder.fromPropertyFile(githubConfigPath.toString()).build()
+    val results = github.myself.listRepositories()
+
+    println(results.mapNotNull { ghRepository -> ghRepository.language }.joinToString(","))
 }
