88e375fbd468a1c337f9c3d749b3b2f276c756d1
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-03-26 14:30:36 +0100

    Minor changes

--- src/main/kotlin/parsers/maven/MavenParser.kt
+++ src/main/kotlin/parsers/maven/MavenParser.kt
@@ -9,13 +9,13 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 class MavenParser(){
     companion object{
-        internal val kotlinXmlMapper = XmlMapper(JacksonXmlModule().apply {
+        private val kotlinXmlMapper = XmlMapper(JacksonXmlModule().apply {
 //        setDefaultUseWrapper(false)
         }).registerKotlinModule()
             .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-        internal inline fun <reified T: Any> parseAs(path: String) : T {
+        private inline fun <reified T: Any> parseAs(path: String) : T {
             val resource = javaClass.classLoader.getResource(path)
             return kotlinXmlMapper.readValue(resource)
         }
@@ -26,6 +26,7 @@ class MavenParser(){
             val pomFile = parseAs<POMProject>("poms/example-pom.xml")
             pomFile.dependencies.forEach { println(it) }
             pomFile.dependencies.forEach { println(generateUrl(it)) }
+
 
             // Get Url
             val url = "https://repo1.maven.org/maven2/org/javatuples/javatuples/1.2/javatuples-1.2.pom"
@@ -38,16 +39,10 @@ class MavenParser(){
 const val ROOT_MAVEN_URL = "https://repo1.maven.org/maven2/";
 
 fun generateUrl(pomProject: POMDependency) : String {
+    // TODO Use a URLBuilder
     return ROOT_MAVEN_URL +
             pomProject.groupId!!.replace(".", "/") + "/" +
             pomProject!!.artifactId + "/" +
             pomProject!!.version + "/" +
             pomProject!!.artifactId + "-" + pomProject!!.version + ".pom";
 }
-
-
-data class POMFile(val libId: LibId, val libConfig: LibConfig?, val libIssue: LibIssue)
-
-data class LibId(val groupId : String, val artifactId: String, val version : String)
-data class LibConfig(val connection : String, val url: String)
-data class LibIssue(val system : String, val url: String)
